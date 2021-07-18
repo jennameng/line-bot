793dafe1bb3a2cd919051af43a3d774c8c8f17d6
Jenna Meng <jenna.meng@gmail.com>
2021-07-18 13:20:02 +0200

    update

--- app.py
+++ app.py
@@ -37,9 +37,20 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    msg = event.message.text
+    r = '不好意思。你在說什麼?'
+    if msg in ['hi', 'Hi', "HI"]:
+        r = '哈囉你好~'
+    elif msg = '你吃飽了嗎?':
+        r = '還沒'
+    elif msg = '你是誰?':
+        r = '我是機器人' 
+    elif '訂位' in msg:
+        r = '你要訂位嗎?'
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=r))
 
 
 if __name__ == "__main__":
